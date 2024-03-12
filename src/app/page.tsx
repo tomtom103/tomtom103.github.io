2ead157abe7debee7dcf1bfa725b851c6ce71510
Thomas Caron <thomas.caron00@gmail.com>
2024-03-11 22:52:54 -0400

    Test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,6 +106,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
+          <p>Hello!</p>
         </a>
       </div>
     </main>
