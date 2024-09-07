65a6d3a46877797ecc3f89908bddfb899a3f7063
Thomas Caron <thomas.caron00@gmail.com>
2024-09-07 11:07:40 -0400

    Remove GH pages

--- src/core/Router.tsx
+++ src/core/Router.tsx
@@ -2,10 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 import InProgress from '@components/InProgress';
 
 export default function Router() {
-    return (
-        <Routes>
-            {/* TODO: Add routes here */}
-            <Route path="*" element={<InProgress />}/>
-        </Routes>
-    )
+  return (
+    <Routes>
+      {/* TODO: Add routes here */}
+      <Route path="*" element={<InProgress />} />
+    </Routes>
+  );
 }
